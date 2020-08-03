ab9558318c287e10c5759065e293bbdcde70d759
Akira Matsuda <ronnie@dio.jp>
2020-08-03 09:37:48 +0900

    UNDS as a 1st class citizen

--- keyboards/claw44/keymaps/amatsuda/keymap.c
+++ keyboards/claw44/keymaps/amatsuda/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
      KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
   //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
-                       KC_A_DEL, KC_G_EN, KC_L_SPC, KC_C_BS,     KC_C_BS, KC_R_ENT, KC_G_JA, KC_A_DEL
+                       KC_A_DEL, KC_G_EN, KC_L_SPC, KC_C_BS,     KC_C_BS, KC_R_ENT, KC_G_JA, KC_UNDS
   //                 `----------+--------+---------+--------'   `--------+---------+--------+---------'
   ),
 
