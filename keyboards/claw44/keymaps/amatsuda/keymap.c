4a0c69e664bba47968f303b4a9683ed59b27a3ce
Akira Matsuda <ronnie@dio.jp>
2020-08-05 01:13:33 +0900

    Just because we got a space for it

--- keyboards/claw44/keymaps/amatsuda/keymap.c
+++ keyboards/claw44/keymaps/amatsuda/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
      KC_GRV , KC_BSLS, KC_CIRC, KC_EXLM, KC_AMPR, KC_PIPE,     KC_AT  , KC_EQL , KC_PLUS, KC_ASTR, KC_PERC, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-     _______, KC_HASH, KC_DLR , KC_DQT , KC_QUOT, KC_TILD,     KC_LEFT, KC_DOWN,  KC_UP , KC_RGHT,COLN_SCLN, _______,
+     _______, KC_HASH, KC_DLR , KC_DQT , KC_QUOT, KC_TILD,     KC_LEFT, KC_DOWN,  KC_UP , KC_RGHT,COLN_SCLN,KC_SCLN,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
      KC_LPRN, _______, _______, _______, KC_LCBR, KC_LBRC,     KC_RBRC, KC_RCBR, _______, _______, _______, KC_RPRN,
   //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
