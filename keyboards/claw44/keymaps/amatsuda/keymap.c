9e8197893311855ebd26c5f58ab16fbd6eb7fdf6
Akira Matsuda <ronnie@dio.jp>
2020-08-09 20:32:53 +0900

    Let's do the LSPO and RSPC

--- keyboards/claw44/keymaps/amatsuda/keymap.c
+++ keyboards/claw44/keymaps/amatsuda/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
      CTL_ESC, KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_ENT , KC_QUOT,
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-     KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
+     KC_LSPO, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSPC,
   //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
                        ALT_BS  , GUI_EN , KC_L_SPC, LOWER_EN,    RAISE_JA,KC_R_ENT, GUI_JA , KC_UNDS
   //                 `----------+--------+---------+--------'   `--------+---------+--------+---------'
